490ac4839db68d515ff9fd6d308086473c7d8c65
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-11 22:35:32 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,8 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(params[:pet])
+    @pet = Pet.create(name: params[:pet_name])
+    @pet.owner = Owner.find_by_id(params[:owner_id])
     if !params[:owner_name].empty?
       @pet.owner = Owner.create(params[:owner_name])
     end
