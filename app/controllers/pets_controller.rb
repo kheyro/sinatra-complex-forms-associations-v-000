5f271b18e4f5192389dfc0f99a4dec5c5ffa5639
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-11 22:29:17 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,10 +11,11 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    pet = Pet.create(params[:pet])
+    @pet = Pet.create(params[:pet])
     if !params[:owner_name].empty?
-      pet.owner = Owner.create(params[:owner_name])
+      @pet.owner = Owner.create(params[:owner_name])
     end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
